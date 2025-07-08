3ef76706148ca508c52dd5fa95465810e284990c
Vu Do <vu@joinself.com>
2025-07-08 17:44:14 +0700

    add advanced notification example

---
+++ examples/server/04_advanced_features/03_notifications/java/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+    application
+}
+
+repositories {
+    mavenCentral()
+    maven("https://central.sonatype.com/repository/maven-snapshots/")
+}
+
+dependencies {
+    implementation("com.joinself:sdk-jvm:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+}
+
+kotlin {
+    sourceSets.main {
+        kotlin.srcDirs(".", "../../../common")
+    }
+    jvmToolchain(17)
+}
+
+application {
+    mainClass = "MainKt"
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
+}
